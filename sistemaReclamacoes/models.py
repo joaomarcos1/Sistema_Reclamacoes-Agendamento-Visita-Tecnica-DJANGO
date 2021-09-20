8fd6765b583cf65f4bea317cf4af6a86c1449a6a
joaomarcos1 <rosaljoao@gmail.com>
2021-09-19 23:54:06 -0300

    Criando Arquivos e aplicação inicial

---
+++ sistemaReclamacoes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib import auth
+import re
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.core import validators
+
+# Create your models here.
