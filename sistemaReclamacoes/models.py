2c29593f60af28316ec63c0a1ff501ff796f64e4
joaomarcos1 <rosaljoao@gmail.com>
2021-09-21 17:02:31 -0300

    alterando models.py

--- sistemaReclamacoes/models.py
+++ sistemaReclamacoes/models.py
@@ -6,3 +6,30 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.core import validators
 
 # Create your models here.
+class Tecnico_Campo(models.Model):
+    id = models.TextField()
+    nome = models.TextField()
+    telefone = models.TextField()
+
+    def setId(self, ident):
+        self.id = ident
+
+    def getId(self, ident):
+        return self.id
+
+    def setNome(self, nome):
+        self.nome = nome
+
+    def getNome(self):
+        return self.nome
+
+    def setTelefone(self,telefone):
+        self.telefone = telefone
+
+    def getTelefone(self):
+        return self.telefone
+
+
+
+
+
