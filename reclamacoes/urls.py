2f18e37f47e20239b46bab0488aeface3db1783d
joaomarcos1 <rosaljoao@gmail.com>
2021-11-05 10:43:09 -0300

    Atualização de arquivos (Inserindo templates)

--- reclamacoes/urls.py
+++ reclamacoes/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 
 urlpatterns = [
+    url(r'^', include ('sistemaReclamacoes.urls')),
     path('admin/', admin.site.urls),
 ]
