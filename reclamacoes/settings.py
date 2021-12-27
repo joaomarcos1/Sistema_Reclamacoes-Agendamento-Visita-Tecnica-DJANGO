853d134cd819485996095624bd8c6d60ae84208d
Lucas Sousa <lucas103101@gmail.com>
2021-12-27 19:22:48 -0300

    add static and media urls

--- reclamacoes/settings.py
+++ reclamacoes/settings.py
@@ -118,5 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/var/wwww/html/relamacoes/static/'
+MEDIA_URL = '/var/wwww/html/relamacoes/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
